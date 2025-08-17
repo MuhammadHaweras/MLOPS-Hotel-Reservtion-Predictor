4d196605c75084d77157731bfe10d46feb96493e
MuhammadHaweras <haweras.noor@gmail.com>
2025-08-17 05:07:43 +0500

    setup jenkins and docker

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -8,8 +8,8 @@ class CustomException(Exception):
     
 
   @staticmethod
-  def get_detailed_error_message(error_message) -> str:
-    _, _, exc_tb = traceback.sys.exc_info()
+  def get_detailed_error_message(error_message, error_detail: sys) -> str:
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     detailed_error_message = f"Error occurred in file: {file_name} at line number: {line_number}, error message: {error_message}"
